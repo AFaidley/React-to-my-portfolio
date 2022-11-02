3645324c83370b711b1cdb437f8fa9333c1fa5da
AFaidley <alviva.faidley@gmail.com>
2022-11-02 16:34:15 -0600

    Added is active on all navbar li

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,26 +3,27 @@ import { NavLink } from 'react-router-dom'
 
 
 function Navbar(props) {
-let thing = {
-    bing: {
-        backgroundColor: "red"
+let color = {
+    active: {
+        backgroundColor: "green"
     },
-
-    that: {
-        color: "blue",
-        
-    }
 }
 
     return (
         <nav className="App">
             <ul>
                 <li> <NavLink to='/' style={({ isActive }) => ({
-          ...(isActive ? thing.bing : null),
+          ...(isActive ? color.active : null),
         })}>About Me</NavLink></li>
-                <li> <NavLink to='/Projects'>Projects</NavLink></li>
-                <li> <NavLink to='/Contact'>Contact Me</NavLink></li>
-                <li> <NavLink to='/Resume'>Resume</NavLink></li>
+                <li> <NavLink to='/Projects' style={({ isActive }) => ({
+          ...(isActive ? color.active : null),
+        })}>Projects</NavLink></li>
+                <li> <NavLink to='/Contact' style={({ isActive }) => ({
+          ...(isActive ? color.active : null),
+        })}>Contact Me</NavLink></li>
+                <li> <NavLink to='/Resume' style={({ isActive }) => ({
+          ...(isActive ? color.active : null),
+        })}>Resume</NavLink></li>
             </ul>
         </nav>
     )
