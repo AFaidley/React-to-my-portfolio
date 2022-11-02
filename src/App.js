f91a1b5a25eeb8e2520f134bf6b9ba1368a5b85d
AFaidley <alviva.faidley@gmail.com>
2022-11-01 18:32:14 -0600

    Started on portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import myPic from './img/me.jpg'
+
+// className="App-logo"
+/* <a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+  >
+  Learn React
+</a> */
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Hi, I'm Alviva. Welcome to my Portfolio!</h1>
+        <img src={myPic} className='mePic' alt="logo" />
       </header>
+     <div className= 'container'></div>
+     <footer>Made with love by Alviva</footer>
     </div>
   );
 }
