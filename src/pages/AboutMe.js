4bad216e34e021bbc0b02143cdd3efef7285ba0d
AFaidley <alviva.faidley@gmail.com>
2022-11-02 16:21:14 -0600

    Fixed my img not showing up on home/about me

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,19 +1,20 @@
 import React from "react";
+import myPic from '../img/me.jpg';
 
-function AboutMe(props) {
+function AboutMe() {
 
     return (
         <section>
             <div>
                 <h2>About Me</h2>
                 <br></br>
-                <img src={props.myPic} className='mePic' alt='me' />
+                <img src={myPic} className='mePic' alt='me' />
                 <p>How about I tell you a little about myself? I am a Full-Stack Software Developer based in Longmont, CO.
                     <br></br>
                     I am currently looking for a company with a dynamic environment and an exciting team so I can continue to nourish
                     my passion to code!
                 </p>
-                <p>If you want any more information about myself or my work, you can always reach out and say hi! &#128075</p>
+                <p>If you want any more information about myself or my work, you can always reach out and say hi!</p>
             </div>
         </section>
     )
