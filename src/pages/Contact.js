9f6a500fd6f4bbe6f0ac5d529891f5931cbd87df
AFaidley <alviva.faidley@gmail.com>
2022-11-03 20:18:19 -0600

    Added proficiencies after rechecking readme requirements- still need to do email check in contact form and github etc footer

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -25,6 +25,27 @@ function Contact() {
                 <br></br>
                 <input type="button" value="Send"/>
             </form>
+
+            <h3>Front-end Proficiencies</h3>
+            <ul className='list-group'>
+                <li className='list-group-item'>HTML</li>
+                <li className='list-group-item'>CSS</li>
+                <li className='list-group-item'>JavaScript</li>
+                <li className='list-group-item'>Responsive Design</li>
+                <li className='list-group-item'>JQuery</li>
+                <li className='list-group-item'>Bootstrap</li>
+                <li className='list-group-item'>React</li>
+            </ul>
+            <h3>Back-end Proficiencies</h3>
+            <ul className='list-group'>
+                <li className='list-group-item'>APIs</li>
+                <li className='list-group-item'>Node</li>
+                <li className='list-group-item'>Express</li>
+                <li className='list-group-item'>MySQL, Sequelize</li>
+                <li className='list-group-item'>REST</li>
+                <li className='list-group-item'>MongoDB, Mongoose</li>
+                <li className='list-group-item'>GraphQL</li>
+            </ul>
         </div>
     );
 }
