97222c8c319002e496904b71032dff418c5dce81
AFaidley <alviva.faidley@gmail.com>
2022-11-07 18:16:34 -0700

    Updates

---
+++ precache-manifest.78746c80a0e603965e241c480aff4c19.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "b361f52a063ab58fc052",
+    "url": "/React-to-my-portfolio/static/css/main.5d23bfcc.chunk.css"
+  },
+  {
+    "revision": "b361f52a063ab58fc052",
+    "url": "/React-to-my-portfolio/static/js/main.b361f52a.chunk.js"
+  },
+  {
+    "revision": "68d85c9b0ba336868cc8",
+    "url": "/React-to-my-portfolio/static/js/1.68d85c9b.chunk.js"
+  },
+  {
+    "revision": "a3e24eeaf03147dc1351",
+    "url": "/React-to-my-portfolio/static/css/2.9411988c.chunk.css"
+  },
+  {
+    "revision": "a3e24eeaf03147dc1351",
+    "url": "/React-to-my-portfolio/static/js/2.a3e24eea.chunk.js"
+  },
+  {
+    "revision": "303a757805546c60ca68",
+    "url": "/React-to-my-portfolio/static/js/runtime~main.303a7578.js"
+  },
+  {
+    "revision": "c37bdfc0e0bace6f4ec3fd352ff14cfd",
+    "url": "/React-to-my-portfolio/static/media/me.c37bdfc0.jpg"
+  },
+  {
+    "revision": "4adf4f42ad9b6fb5d7075d093be82182",
+    "url": "/React-to-my-portfolio/static/media/screenshot-main.4adf4f42.jpg"
+  },
+  {
+    "revision": "854f3848be39781722aef54b4789cd8e",
+    "url": "/React-to-my-portfolio/static/media/air-quality-home-page.854f3848.jpg"
+  },
+  {
+    "revision": "d46d3e0fd27795c36e1a727c974345d4",
+    "url": "/React-to-my-portfolio/static/media/NoteTaker.d46d3e0f.png"
+  },
+  {
+    "revision": "f1d1eee1904a90493e53af85bd2ed32b",
+    "url": "/React-to-my-portfolio/static/media/Screenshot 2022-07-24 153656.f1d1eee1.png"
+  },
+  {
+    "revision": "3f2a75f102f6d56e43e378543eeff6c4",
+    "url": "/React-to-my-portfolio/static/media/Screenshot 2022-07-29 145947.3f2a75f1.png"
+  },
+  {
+    "revision": "c084a9701f91350ed35e5d4219b317c6",
+    "url": "/React-to-my-portfolio/static/media/JATE.c084a970.jpg"
+  },
+  {
+    "revision": "23a827d9fc64a23fce8bf96756effd64",
+    "url": "/React-to-my-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8c0150ce9fbf3812d698aba5f53da61.js"
+  "/React-to-my-portfolio/precache-manifest.78746c80a0e603965e241c480aff4c19.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/React-to-my-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
