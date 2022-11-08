f0de25f877707b153c2fdf62ac27813ac3f788d4
AFaidley <alviva.faidley@gmail.com>
2022-11-07 18:11:59 -0700

    Updates

---
+++ precache-manifest.e8c0150ce9fbf3812d698aba5f53da61.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "b361f52a063ab58fc052",
+    "url": "/static/css/main.5d23bfcc.chunk.css"
+  },
+  {
+    "revision": "b361f52a063ab58fc052",
+    "url": "/static/js/main.b361f52a.chunk.js"
+  },
+  {
+    "revision": "68d85c9b0ba336868cc8",
+    "url": "/static/js/1.68d85c9b.chunk.js"
+  },
+  {
+    "revision": "a3e24eeaf03147dc1351",
+    "url": "/static/css/2.9411988c.chunk.css"
+  },
+  {
+    "revision": "a3e24eeaf03147dc1351",
+    "url": "/static/js/2.a3e24eea.chunk.js"
+  },
+  {
+    "revision": "92a09edd18b8e2c9245f",
+    "url": "/static/js/runtime~main.92a09edd.js"
+  },
+  {
+    "revision": "c37bdfc0e0bace6f4ec3fd352ff14cfd",
+    "url": "/static/media/me.c37bdfc0.jpg"
+  },
+  {
+    "revision": "4adf4f42ad9b6fb5d7075d093be82182",
+    "url": "/static/media/screenshot-main.4adf4f42.jpg"
+  },
+  {
+    "revision": "854f3848be39781722aef54b4789cd8e",
+    "url": "/static/media/air-quality-home-page.854f3848.jpg"
+  },
+  {
+    "revision": "d46d3e0fd27795c36e1a727c974345d4",
+    "url": "/static/media/NoteTaker.d46d3e0f.png"
+  },
+  {
+    "revision": "f1d1eee1904a90493e53af85bd2ed32b",
+    "url": "/static/media/Screenshot 2022-07-24 153656.f1d1eee1.png"
+  },
+  {
+    "revision": "3f2a75f102f6d56e43e378543eeff6c4",
+    "url": "/static/media/Screenshot 2022-07-29 145947.3f2a75f1.png"
+  },
+  {
+    "revision": "c084a9701f91350ed35e5d4219b317c6",
+    "url": "/static/media/JATE.c084a970.jpg"
+  },
+  {
+    "revision": "689355f40f695397f3c2b42582ff472d",
+    "url": "/index.html"
+  }
+];
