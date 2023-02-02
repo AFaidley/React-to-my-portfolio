f6022d9e59eef535bc47c0053339d130984a5e55
AFaidley <alviva.faidley@gmail.com>
2023-02-01 20:58:10 -0700

    Worked on about me and resume pages

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,30 +1,43 @@
-import React from "react";
+import React from 'react';
 import myPic from '../img/me.jpg';
 
 function AboutMe() {
-
-    return (
-        <section className="body" >
-            <div className="text-center body">
-                <h2>About Me</h2>
-                <br></br>
-                <img src={myPic} className='mePic' alt='me' />
-                <br></br>
-                <br></br>
-                <br></br>
-                <p className='aboutMe'>How about I tell you a little about myself? I am a Full-Stack Software Developer based in Longmont, CO.
-                    <br></br>
-                    In my spare time you will find me outside with my dogs, gaming or creating random apps!
-                    <br></br>
-                    I am currently looking for a company with a dynamic environment and an exciting team so I can continue to nourish
-                    my passion to code!
-                </p>
-                <p className='aboutMe'>If you want any more information about myself or my work, you can always reach out and say hi!</p>
-                <br></br>
-                <br></br>
-            </div>
-        </section>
-    )
+  return (
+    <section className='body'>
+      <div className='text-center body'>
+        <h2>About Me</h2>
+        <br></br>
+        <img src={myPic} className='mePic' alt='me' />
+        <br></br>
+        <br></br>
+        <br></br>
+        <p className='aboutMe'>
+          How about I tell you a little about myself? I am a Full stack web
+          developer with aviation experience within the U.S as well as abroad. I
+          recently earned a certificate in full stack web development from the
+          University of Denver, with newly developed skills in JavaScript, CSS,
+          React.js, MySQL, MongoDB, PWA's, Node and responsive web design. A
+          quick problem solver who can achieve deadlines without sacrificing
+          quality of work. Due to my background in aviation, I am able to work
+          in high pressure and fast paced environments with ease. I possess
+          strong leadership qualities as well as interpersonal skills which help
+          me to work in a highly collaborative atmosphere. With each project, my
+          goal is to develop something that I am proud to call my own while
+          maintaining an impactful and smooth user experience.
+          <br></br>
+          In my spare time you will find me outside with my dogs, gaming or
+          creating random apps!
+          <br></br>
+        </p>
+        <p className='aboutMe'>
+          If you want any more information about myself or my work, you can
+          always reach out and say hi!
+        </p>
+        <br></br>
+        <br></br>
+      </div>
+    </section>
+  );
 }
 
 export default AboutMe;
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,35 +1,42 @@
 import React from 'react';
 
 function Resume() {
-    return (
-        <div className='text-center'>
-            <h2 className='text-center mt-5'><a id='resume'  href='https://docs.google.com/document/d/1SAtbbtRcBFoVcJBXsU-uE4UhwLYojfVvIsfPpshaXlk/edit?usp=sharing'>View my Resume</a></h2>
-            <br></br>
-            <br></br>
-            <h3>Front-end Proficiencies</h3>
-            <ol className='list-group' id='reslist'>
-                <li className='list-group-item'>HTML</li>
-                <li className='list-group-item'>CSS</li>
-                <li className='list-group-item'>JavaScript</li>
-                <li className='list-group-item'>Responsive Design</li>
-                <li className='list-group-item'>JQuery</li>
-                <li className='list-group-item'>Bootstrap</li>
-                <li className='list-group-item'>React</li>
-            </ol>
-            <h3>Back-end Proficiencies</h3>
-            <ul className='list-group' id='reslist'>
-                <li className='list-group-item'>APIs</li>
-                <li className='list-group-item'>Node</li>
-                <li className='list-group-item'>Express</li>
-                <li className='list-group-item'>MySQL, Sequelize</li>
-                <li className='list-group-item'>REST</li>
-                <li className='list-group-item'>MongoDB, Mongoose</li>
-                <li className='list-group-item'>GraphQL</li>
-            </ul>
-            <br></br>
-            <br></br>
-        </div>
-    );
+  return (
+    <div className='text-center'>
+      <h2 className='text-center mt-5'>
+        <a
+          id='resume'
+          href='https://docs.google.com/document/d/1SAtbbtRcBFoVcJBXsU-uE4UhwLYojfVvIsfPpshaXlk/edit?usp=sharing'
+        >
+          Click to view my Resume
+        </a>
+      </h2>
+      <br></br>
+      <br></br>
+      <h3>Front-end Proficiencies</h3>
+      <ol className='list-group' id='reslist'>
+        <li className='list-group-item'>HTML</li>
+        <li className='list-group-item'>CSS</li>
+        <li className='list-group-item'>JavaScript</li>
+        <li className='list-group-item'>Responsive Design</li>
+        <li className='list-group-item'>JQuery</li>
+        <li className='list-group-item'>Bootstrap</li>
+        <li className='list-group-item'>React</li>
+      </ol>
+      <h3>Back-end Proficiencies</h3>
+      <ul className='list-group' id='reslist'>
+        <li className='list-group-item'>APIs</li>
+        <li className='list-group-item'>Node</li>
+        <li className='list-group-item'>Express</li>
+        <li className='list-group-item'>MySQL, Sequelize</li>
+        <li className='list-group-item'>REST</li>
+        <li className='list-group-item'>MongoDB, Mongoose</li>
+        <li className='list-group-item'>GraphQL</li>
+      </ul>
+      <br></br>
+      <br></br>
+    </div>
+  );
 }
 
 export default Resume;
